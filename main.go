47e3952e61e8459959927eec8eec066e39c8a4c6
Dmitriy <dima.pozdneev@yandex.ru>
2024-05-05 18:42:20 +0300

    Add Trapezoid Rule

--- main.go
+++ main.go
@@ -20,12 +20,23 @@ func UseRectangleRule(xmin float64, xmax float64, num_intervals int) float64 {
 	}
 	return total_area
 }
-
+func UseTrapezoidRule(xmin float64, xmax float64, num_intervals int) float64 {
+	dx := (xmax - xmin) / float64(num_intervals)
+	var total_area float64 = 0
+	x := xmin
+	for i := 0; i < num_intervals; i++ {
+		total_area = total_area + dx*(formFunction(x)+formFunction(x+dx))/2
+		x = x + dx
+	}
+	return total_area
+}
 func main() {
 	var xmin float64 = 0
 	var xmax float64 = 5
 	num_intervals := 10
 	area := UseRectangleRule(xmin, xmax, num_intervals)
+	area_trapez := UseTrapezoidRule(xmin, xmax, num_intervals)
 	fmt.Println("Площадь рассчитанная по методу прямоугольников: ", area)
+	fmt.Println("Площадь рассчитанная по методу трапеций: ", area_trapez)
 
 }
